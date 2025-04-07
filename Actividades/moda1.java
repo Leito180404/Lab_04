4b7819f5bc8feaf531301949045ba99510947747
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-04-07 08:06:04 -0500

    .

--- Actividades/moda1.java
+++ Actividades/moda1.java
@@ -30,6 +30,33 @@ public class moda1 {
         return moda;
     }
 
+    public static int moda2(int array[]) {
+        int first = 1;
+        int p = 0;
+        int end = array.length - 1;
+        int moda = array[0];
+        int frec = 1;
+        int maxfrec = 0;
+    
+        while (first <= end) {
+            if (array[p] == array[first]) {
+                frec++;
+                first++;
+            } else {
+                if (frec > maxfrec) {
+                    maxfrec = frec;
+                    moda = array[p];
+                }
+                p = first;
+                first = p + 1;
+                frec = 1;
+            }
+        }
+    
+        return moda;
+    }
+    
+
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 3, 2, 3, 1};
 
@@ -41,7 +68,11 @@ public class moda1 {
         System.out.println("El número " + num + " aparece " + freq + " veces.");
 
         // Ejemplo de llamada al método modal1
-        int moda = modal1(array);
-        System.out.println("La moda del arreglo es: " + moda);
+        int moda1Result = modal1(array);
+        System.out.println("La moda del arreglo utilizando modal1 es: " + moda1Result);
+
+        // Ejemplo de llamada al método moda2
+        int moda2Result = moda2(array);
+        System.out.println("La moda del arreglo utilizando moda2 es: " + moda2Result);
     }
 }    
