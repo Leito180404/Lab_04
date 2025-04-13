0a0911e5a80cd2f9dc830bd93c32fce80784c635
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-04-12 21:57:56 -0500

    Update ejercicio2.java

--- Ejercicios/ejercicio2.java
+++ Ejercicios/ejercicio2.java
@@ -1,47 +1,67 @@
 package Ejercicios;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ejercicio2 {
-        public static int partition(int[] arr, int low, int high) {
-            int pivot = arr[high];
-            int i = low - 1;
 
-            for (int j = low; j < high; j++) {
-                if (arr[j] <= pivot) {
-                    i++;
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+    public static int particion(ArrayList<Integer> arr, int low, int high) {
+        int pivot = arr.get(high);
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr.get(j) <= pivot) {
+                i++;
+                int temp = arr.get(i);
+                arr.set(i, arr.get(j)); 
+                arr.set(j, temp);
             }
-
-            int temp = arr[i + 1];
-            arr[i + 1] = arr[high];
-            arr[high] = temp;
-            
-            return i + 1; 
         }
 
-        public static int quickSelect(int[] arr, int low, int high, int k) {
-            if (low <= high) {
+        int temp = arr.get(i + 1);
+        arr.set(i + 1, arr.get(high));
+        arr.set(high, temp);
+        
+        return i + 1; 
+    }
 
-                int pivotIndex = partition(arr, low, high);
-                if (pivotIndex == k) {
-                    return arr[pivotIndex];
-                } 
-                else if (pivotIndex > k) {
-                    return quickSelect(arr, low, pivotIndex - 1, k);
-                } 
-                else {
-                    return quickSelect(arr, pivotIndex + 1, high, k);
-                }
+    public static int quickSelect(ArrayList<Integer> arr, int low, int high, int k) {
+        if (low <= high) {
+
+            int pivotIndex = particion(arr, low, high);
+            if (pivotIndex == k) {
+                return arr.get(pivotIndex); 
+            } 
+            else if (pivotIndex > k) {
+                return quickSelect(arr, low, pivotIndex - 1, k);
+            } 
+            else {
+                return quickSelect(arr, pivotIndex + 1, high, k);
             }
-            return -1;
         }
+        return -1;
+    }
     
         public static void main(String[] args) {
-            int[] arr1 = {4, 2, 7, 10, 4, 17};
-            int k = 3;
-            System.out.println("El " + k + "-esimo elemento mas pequenio es: " + quickSelect(arr1, 0, arr1.length - 1, k - 1));
-            int[] arr2 = {4, 2, 7, 10, 4, 17};
+            Scanner sc = new Scanner(System.in);
+            while (true){
+                System.out.println("ingrese el tamanio del arreglo: ");
+                int n = sc.nextInt();
+                System.out.println("ahora ingrese los elementos: ");
+                ArrayList<Integer> arr = new ArrayList<>(n);
+                for(int i = 0; i<n; i++){
+                    System.out.print("Elemento " + (i + 1) + ": ");
+                    arr.add(sc.nextInt()); 
+                }
+                System.out.println("ahora ingrese el valor de k: ");
+                int k = sc.nextInt();
+                System.out.println("El " + k + "-esimo elemento mas pequenio es: " + quickSelect(arr, 0, arr.size() - 1, k - 1));
+                
+                System.out.println("¿Deseas ingresar otro arreglo? (si/no): ");
+                String respuesta = sc.next();
+                if (respuesta.equalsIgnoreCase("no")) {
+                    System.out.println("Saliendo...");
+                    break; 
+            }
+            sc.close();
         }
     }
+}
