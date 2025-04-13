d146b406274a9565de867261566d6763405d7b3b
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-04-12 23:23:16 -0500

    Update potencias.java

--- Ejercicios/potencias.java
+++ Ejercicios/potencias.java
@@ -42,16 +42,25 @@ public class potencias {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        while (true){
         System.out.print("¿Cuántos números quieres ingresar en el arreglo? ");
         int n = sc.nextInt();
         List<Integer> arr = new ArrayList<>(n);
-
+        
+        System.out.println("Ahora ingrese los elementos: ");
         for (int i = 0; i < n; i++) {
-            System.out.print("Ingresa el número " + (i + 1) + ": ");
+            System.out.print("Elemento " + (i + 1) + ": ");
             arr.add(sc.nextInt());
         }
 
         System.out.println(resolverProblema(arr));
         sc.close();
+        System.out.println("¿Deseas ingresar otro arreglo? (si/no): ");
+        String respuesta = sc.next();
+        if (respuesta.equalsIgnoreCase("no")) {
+            System.out.println("Saliendo...");
+            break; 
+        }
     }
+}
 }
